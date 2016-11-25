894dce1db677fd6adbb8b434e8aaa1acc515d7ff
luohao <553569682@qq.com>
2016-11-25 17:35:05 +0800

    mybatis

---
+++ model/src/main/java/com/github/wx/gadget/dbo/Test.java
@@ -0,0 +1,7 @@
+package com.github.wx.gadget.dbo;
+
+/**
+ * Created by luohao on 2016/11/25.
+ */
+public class Test {
+}
---
+++ service/src/main/java/com/github/wx/gadget/mybatis/config/MyBatisMapperScannerConfig.java
@@ -0,0 +1,23 @@
+package com.github.wx.gadget.mybatis.config;
+
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by luohao on 2016.11.25.
+ */
+@Configuration
+@AutoConfigureAfter(MyBatisConfig.class)
+public class MyBatisMapperScannerConfig {
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        mapperScannerConfigurer.setBasePackage("com.github.wx.gadget.mybatis.mapper");
+        return mapperScannerConfigurer;
+    }
+
+}
